17efa92e9b239627e9ce642086f09bb50a9d25ba
Gresh Basic <basic.gresham@gmail.com>
2023-09-12 18:47:07 -0500

    finished lab

--- labs/lab01.ml
+++ labs/lab01.ml
@@ -1,11 +1,16 @@
-let foo x y
-  = x + y
 
-let plus x y
-  = string_of_int x ^ " plus " ^
+let multiply x y
+  = string_of_int x ^ " multiplied by " ^
     string_of_int y ^ " is " ^
-    string_of_int (foo x y)
+    string_of_int (x * y)
 
-let user_number_x = read_int ()
-let user_number_y = read_int ()
-let () = print_endline(plus user_number_x user_number_y)
+
+let multStringTable n =
+  let rec loop i =
+    if i > n then []
+    else (multiply n i) :: loop (i+1)
+  in loop 1
+
+
+let user_number = read_int ()
+let mult_table = multStringTable(user_number)
