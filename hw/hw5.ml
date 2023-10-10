065d685ec6b64e63154b8aec7cae0c63aec5a797
Nebula <basic.gresham@gmail.com>
2023-10-10 12:30:28 -0500

    sending hw5 up

---
+++ hw/hw5.ml
@@ -0,0 +1,23 @@
+type tree =
+  | Leaf
+  | Node of int * tree * tree
+
+
+let with_default default op =
+  failwith "TODO"
+
+
+module Tree_ops =
+struct 
+  let rec sum t =
+    failwith "TODO"
+                                      
+  let rec tmax t =
+    failwith "TODO"
+          
+  let rec flatten t =
+    failwith "TODO"
+                                            
+  let rec is_tree_sorted t =
+    failwith "TODO"
+end
